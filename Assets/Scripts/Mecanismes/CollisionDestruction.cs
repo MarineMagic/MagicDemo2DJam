63452567f600bcf6180c381a4409e6e76a8d28ea
Marine <marine.bocquet@magicmakers.fr>
2025-02-12 13:29:54 +0100

    Re-texturing

--- Assets/Scripts/Mecanismes/CollisionDestruction.cs
+++ Assets/Scripts/Mecanismes/CollisionDestruction.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CollisionDestruction : MonoBehaviour
 {
+    public float delai = 0.1f;
     // Cette fonction est appelée lorsqu'une collision se produit
     void OnCollisionEnter2D(Collision2D collision) {
         // Détruit cet objet 0.1 seconde après la collision
-        Destroy(this.gameObject, 0.1f);
+        Destroy(this.gameObject, delai);
     }
 }
